8c1c4119ac844b188450dbcd20e114e5bbc551da
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2024-07-18 13:50:47 +0100

    Noteballs Data, Methods, Child Components, Computed & Emit

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div class="card mb-4">
+        <div class="card-content">
+            <div class="content">
+                {{ note.content }}
+                <div class="has-text-right has-text-grey-light">
+                    <small>
+                        {{ characterLength }} 
+                    </small>
+                </div>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" class="card-footer-item" @click.prevent="deleteNote">Delete</a>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+
+const props = defineProps({
+    note:{
+        type: Object,
+        required: true
+    }
+})
+
+const characterLength = computed(()=>{
+    let noteContentCount = props.note.content.length
+    let description = noteContentCount > 1 ? 
+    "characters" : "character";
+    return `${noteContentCount} ${description}`;
+})
+
+const emit = defineEmits(['delete']);
+
+const deleteNote = ()=>{
+    emit('delete', props.note.id);
+}
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,31 +3,60 @@
         <div class="card has-backgroud-success-dark p-4 mb-5">
 
             <div class="field">
-            <div class="control">
-                <textarea class="textarea" placeholder="Textarea"></textarea>
-            </div>
+                <div class="control">
+                    <textarea ref="newNoteRef" v-model="newNote" class="textarea" placeholder="Textarea"></textarea>
+                </div>
             </div>
 
             <div class="field is-grouped is-grouped-right">
                 <div class="control">
-                    <button class="button is-link has-backgroud-success">
+                    <button 
+                        :disabled="!newNote"
+                        @click="postNewNoteHandler" class="button is-link has-backgroud-success">
                         Add New Notes</button>
                 </div>
             </div>
 
         </div>
-        <div 
-            v-for="i in 3"
-            class="card mb-4" >
-            <div class="card-content">
-                <div class="content">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo temporibus ipsum, tempora natus eaque debitis voluptatibus maxime magnam iusto repellendus minus, maiores ratione rerum sapiente ut possimus aliquid alias dolorum!
-                </div>
-            </div>
-            <footer class="card-footer">
-                <a href="#" class="card-footer-item">Edit</a>
-                <a href="#" class="card-footer-item">Delete</a>
-            </footer>
-        </div>
+        
+        <Note v-for="note in notes" :key="note.id" @delete="deleteNote"
+            :note="note"></Note>
+        
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import Note from '@/components/Notes/Note.vue';
+
+
+const newNote = ref('');
+const newNoteRef = ref(null);
+const postNewNoteHandler = () => {
+    let currentDate = new Date().getTime(),
+        id = currentDate.toString();
+
+    notes.value.unshift({ id, content: newNote.value });
+    
+    newNote.value = ''
+
+    newNoteRef.value.focus()
+
+}
+const notes = ref([
+    {
+        id: "1",
+        content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo temporibus ipsum, tempora natus eaque debitis voluptatibus maxime magnam iusto repellendus minus, maiores ratione rerum sapiente ut possimus aliquid alias dolorum',
+    },
+    {
+        id: "2",
+        content: 'na second note be this',
+    },
+]);
+const deleteNote = (id)=>{
+
+    let index = notes.value.findIndex((note)=> note.id==id);
+    if(index !== -1) notes.value.splice(index,1)
+
+}
+</script>
